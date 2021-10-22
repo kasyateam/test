d35559a68c66ea25dfcae54a65e1c8e382419149
Kasya Team <70856891+kasyateam@users.noreply.github.com>
2021-10-22 23:52:21 +0600

    Add files via upload

--- k.feed/k-feed.js
+++ k.feed/k-feed.js
@@ -1,111 +1,54 @@
-//Та самая панель переключения
-
-var on = 0 //Включение уведомлений
+window.location.hash
 
 
-//Тестовое окно
-var title = ''
-var post_info = ''
-var head = ''
-var u_title = 'Обновление'
-var text_info = 'Создание динамического контента и обновление Kasya UI'
-var head = ''
+//Уведомления
+var info_h = 'Обновление K.Feed'
+var info_t = 'Демонстрация Kasya UI третьего поколения<br>Вам скоро будет необхожимо обновить приложение'
 
-//Элементы
-var uved = '<div id="info-all" class="info"><h3 id="info-1">'+u_title+'</h3><p id="info-2">'+text_info+'</p></div>'
-var post = ' <div id="post" class="post"><div id="post-avtor" class="post-avtor">'+title+'</div><div id="post-info" class="post-info">'+post_info+'</div><div class="post-time">kasyateam</div></div>'
-var head = '<div id="header"class="header"><p id="header-text" class="header-text">'+head+'</p></div>'
-var del = '<hr><div class="end">Больше ничего нет :(</div>'
-
-
-//автопостройка шапки
-document.head.innerHTML = '<meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=Edge"><meta name="viewport" content="width=device-width, initial-scale=1"><title></title><link rel="stylesheet" href="style.css">'
+var loading = '<div id="load"><div id="load_1"><img id="load_2" src="load.gif"></div>'
 
 
 
-
-//автопостройка страницы
-document.body.innerHTML = '<main>'+head+post+del+uved+'</main>'
-
-
-
-
-// Уведомления
-if (on == 1){
-    document.getElementById('info-1').innerHTML = 'Обновление';
-    document.getElementById('info-2').innerHTML = 'Создание динамического контента и обновление Kasya UI';    
+if (window.location.hash = '#1'){
+    var post = '<div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс HTML</p></div><a id="button" href="https://kasyateam.github.io/m/html/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс CSS</p></div><a id="button" href="https://kasyateam.github.io/m/css/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс JavaScript</p></div><a id="button" href="https://kasyateam.github.io/m/js/start.html">Перейти</a></div>'
+    var news = '<div id="info"><h3 id="info_1">'+info_h+'</h3><p id="info_2">'+info_t+'</p></div>'
+}else if (window.location.hash = '#2'){
+    var post = '<div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс HTML</p></div><a id="button" href="https://kasyateam.github.io/m/html/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс CSS</p></div><a id="button" href="https://kasyateam.github.io/m/css/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс JavaScript</p></div><a id="button" href="https://kasyateam.github.io/m/js/start.html">Перейти</a></div>'
+    var news = '<div id="info"><h3 id="info_1">'+info_h+'</h3><p id="info_2">'+info_t+'</p></div>'
+}else if (window.location.hash = '#3'){
+    var post = '<div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс HTML</p></div><a id="button" href="https://kasyateam.github.io/m/html/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс CSS</p></div><a id="button" href="https://kasyateam.github.io/m/css/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс JavaScript</p></div><a id="button" href="https://kasyateam.github.io/m/js/start.html">Перейти</a></div>'
+    var news = '<div id="info"><h3 id="info_1">'+info_h+'</h3><p id="info_2">'+info_t+'</p></div>'
 }else{
-    document.getElementById('info-1').remove()
+    window.location.href = '/index'
 }
 
 
 
-//K.Feed m
-
-
-
-//Для K.Feed m
-if (window.location.pathname == '/k.feed/k-feed'){
-
-    document.title = 'K.Feed | Главная';
-    document.getElementById('index-date').innerHTML = 'Добро пожаловать';
-    document.getElementById('index-razdel').innerHTML = 'Выберите раздел :)';
-
-
-
-}else if(window.location.pathname == '/m/feed'){
-
-    document.title = 'K.Feed | Новости';
 
 
 
 
-}else if(window.location.pathname == '/k.feed/k-feed#1'){
-
-    var post_info = '<center><img style="width: 40%" src="https://kasyateam.github.io/assets/img/kasya_stick.png" alt="K.Feed"><img style="width: 60%; border-radius: 20px;" src="https://kasyateam.github.io/assets/img/QR-download.png" alt="K.Feed"></center>'
-    document.title = 'K.Feed';
-    document.getElementById('info-1').innerHTML = 'Это важно';
-    document.getElementById('info-2').innerHTML = 'Данный раздел доступен только в мобильной версии<br/> Наведите камеру на QR код и скачайте K.Feed!';
-    document.body.innerHTML = '<main>'+head+post+del+uved+'</main>'
+//Формирование главного меню
+document.head.innerHTML = '<meta name="viewport" content="width=device-width, initial-scale=1"><link rel="icon" href="https://kasyateam.github.io/favicon.ico" type="image/x-icon"><link rel="stylesheet" href="kasya_UI.css"></link>'
+document.body.innerHTML = '<main id="main" onload="ok()"><div id="load"><div id="load_1"><img id="load_2" src="load.gif"></div></div><div id="head"></div>'+post+'<hr id="hr">'+news+'</main>'
 
 
 
-}else if(window.location.pathname == '/k.feed/k-feed#2'){
-
-    document.title = 'K.Feed | Kasya ID';
-    document.body.innerHTML = '<main>'+head+post+del+uved+'</main>'    
+//Формирование раздела
+document.getElementById('head').innerHTML = 'Курсы по программированию'
 
 
-    document.getElementById('reg').onclick = function(){
-        document.getElementById('kasyaid').innerHTML = 'Для создания ключа, используется электронная почта и пароль. <br><br> Учтите, что мы можем удалить ваш аккаунт, если он выглядит подозрительно. Но перед этим, он будет заморожен. Если вы заметили, что ваш аккаунт не совершает вход и нарушена работа сервиса при совершенном входе, то сообщите в поддержку. <br/><br/>Обратите внимание, что для подтверждения пользователя, перед процессом восстановления, мы сбросим пароль, отправив на электронную почту сброс-ссылку.'  
-    }
+//автофункция OFF
+if (info_t == ''){
+    document.getElementById('info').remove()
+    document.getElementById('hr').remove()
+}
 
-    document.getElementById('res').onclick = function(){
-        document.getElementById('kasyaid').innerHTML = 'Что делать, если вы забыли пароль? <br/><br/>При такой ситуации, так же обратитесь в поддержку, если вход не совершен. <br/><br/>Если у вас совершен вход, но чувствуете, что забыли пароль, то перейдите в раздел "Kasya ID", где можете запросить сброс-ссылку.'
-    }
-
-}else if(window.location.pathname == '/k.feed/k-feed#3'){
-
-    var head = 'Курсы программирования'
-    document.title = 'K.Feed | Курсы';
-    document.body.innerHTML = '<main><div class="post"><div class="post-avtor">HTML</div><div class="post-info">HTML курс <br/><a href="https://kasyateam.github.io/m/html/start.html">Перейти</a></div><div class="post-time"></div></div><div class="post"><div class="post-avtor">CSS</div><div class="post-info">CSS курс <br/><a href="https://kasyateam.github.io/m/css/start.html">Перейти</a></div><div class="post-time"></div></div><div class="post"><div class="post-avtor">JavaScript</div><div class="post-info">JavaScript курс <br/><a href="https://kasyateam.github.io/m/js/start.html">Перейти</a></div><div class="post-time"></div></div></main>'
-}else{
-
+function ok() {
+    setTimeout(document.getElementById('load').remove(), 5000)
 }
 
 
-//Исключение откл элементов:
 
-/*Заголовок
-if(document.getElementById('header-text').innerHTML ==''){
-    document.getElementById('header').remove();
-}
 
-//Постинг
-if(document.getElementById('post-info').innerHTML ==''){
-    document.getElementById('post').remove();
-}
-//Уведомление
-if(document.getElementById('text_info').innerHTML =='' && document.getElementById('').innerHTML =='var u_title' == ''){
-    document.getElementById('post').remove();
-}*/
+
--- k.feed/kasya_UI.js
+++ k.feed/kasya_UI.js
@@ -3,9 +3,15 @@ var info_h = 'Обновление K.Feed'
 var info_t = 'Демонстрация Kasya UI третьего поколения<br>Вам скоро будет необхожимо обновить приложение'
 
 
+
+
 var post = '<div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс HTML</p></div><a id="button" href="https://kasyateam.github.io/m/html/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс CSS</p></div><a id="button" href="https://kasyateam.github.io/m/css/start.html">Перейти</a></div><div id="post"><div id="post_head"><img src="kasya.ico" id="ava"><span id="post_avtor">Kasya Team</span></div><div id="post_text"><p>Курс JavaScript</p></div><a id="button" href="https://kasyateam.github.io/m/js/start.html">Перейти</a></div>'
 var loading = '<div id="load"><div id="load_1"><img id="load_2" src="load.gif"></div>'
 var news = '<div id="info"><h3 id="info_1">'+info_h+'</h3><p id="info_2">'+info_t+'</p></div>'
+
+
+
+
 
 //Формирование главного меню
 document.head.innerHTML = '<meta name="viewport" content="width=device-width, initial-scale=1"><link rel="icon" href="https://kasyateam.github.io/favicon.ico" type="image/x-icon"><link rel="stylesheet" href="kasya_UI.css"></link>'
@@ -13,8 +19,13 @@ document.body.innerHTML = '<main id="main" onload="ok()"><div id="load"><div id=
 
 
 
+
+
+
 //Формирование раздела
 document.getElementById('head').innerHTML = 'Курсы по программированию'
+
+
 
 
 //автофункция OFF
